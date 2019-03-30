dc78a57dfe4f2a43a56e3b1ecb18694e231776a2
Kairat Symanov <symanov79@gmail.com>
2019-03-30 14:21:50 +0600

    Создал сущность Заведения, прописал весь CRUD и вьюхи

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,49 @@
+class PlacesController < ApplicationController
+  before_action :find_place, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @place = Place.new
+  end
+
+  def index
+    @places = Place.all
+  end
+  
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @place.update(place_params)
+      redirect_to @place
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @place.destroy
+      redirect_to places_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :category)
+  end 
+
+  def find_place
+    @place = Place.find(params[:id])
+  end
+end
