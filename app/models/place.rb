fbaecdb799f0a94f83dec79bc4ee8d99c269bfe0
Kairat Symanov <symanov79@gmail.com>
2019-03-30 16:12:53 +0600

    Добавил валидации к моделям

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,5 @@
 class Place < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :category, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { maximum: 500 }
 end
